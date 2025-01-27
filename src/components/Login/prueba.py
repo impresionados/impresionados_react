cfa0e0e85cfdb8d78f8bf69af535c3288ef43ea5
nicolasdavidgilbert <nico@farobag.com>
2025-01-27 09:13:24 +0000

    Base de datos creada

---
+++ src/components/Login/prueba.py
@@ -0,0 +1,3 @@
+pruea = [{"id":1,"descripcion":"hfdjsfjljdf"},{"id":2,"descripcion":"ooooooooooooo"},]
+for prod in pruea:
+    print(prod["id"])
---
+++ src/database/mapeo_colecciones.py
@@ -0,0 +1,55 @@
+import mongoengine
+
+
+class User(mongoengine.Document):
+    meta = {'collection': 'users'}  # To specify the collection being mapped
+
+    # _id = mongoengine.IntField(required=True)
+    user_name = mongoengine.StringField(required=True)
+    email = mongoengine.StringField(required=True)
+    password = mongoengine.StringField(required=True)
+    registration_date = mongoengine.DateTimeField(required=True)
+
+
+class Rating(mongoengine.EmbeddedDocument):  # To specify that it is inside another class
+    user_id = mongoengine.StringField(required=True)
+    rating = mongoengine.IntField(required=True)
+    comment = mongoengine.StringField()
+    date = mongoengine.DateTimeField(required=True)
+
+
+class Super_tipos(mongoengine.Document):
+    # _id = mongoengine.IntField(required=True)
+    meta = {'collection': 'super_tipos'}
+    nombre_super_tipo=mongoengine.StringField(required=True)
+
+
+class Tipos(mongoengine.Document):
+    #_id = mongoengine.IntField(required=True)
+    meta = {'collection': 'tipos'}
+    nombre_tipo = mongoengine.StringField(required=True)
+
+
+class Product(mongoengine.Document):
+    meta = {'collection': 'products'}
+
+
+    name = mongoengine.StringField(required=True)
+    description = mongoengine.StringField()
+    price = mongoengine.FloatField(required=True)
+    stock = mongoengine.IntField(required=True)
+    category = mongoengine.ListField(mongoengine.ReferenceField(Tipos))  # Si Tipos es un Document
+    image = mongoengine.FileField()
+    super_tipo= mongoengine.ReferenceField(Super_tipos)
+    ratings = mongoengine.ListField(mongoengine.EmbeddedDocumentField(Rating))  # It will be a list of Rating objects
+
+
+class Order(mongoengine.Document):
+    meta = {'collection': 'orders'}
+
+    # _id = mongoengine.IntField(required=True)
+    product_id = mongoengine.StringField(required=True)
+    user_id = mongoengine.StringField(required=True)
+    date = mongoengine.DateTimeField(required=True)
+    total = mongoengine.FloatField(required=True)
+    status = mongoengine.StringField(required=True)
